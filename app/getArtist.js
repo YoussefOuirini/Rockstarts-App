9e0526f576bd690b4723767a249eb0d7c5b85b25
Youssef Ouirini <youssef.ouirini-cic.netherlands@ibm.com>
2019-06-16 20:17:08 +0200

    Adding getAllSongs and getAllArtists

--- app/getArtist.js
+++ app/getArtist.js
@@ -3,3 +3,7 @@ const { artists } = require('./database.js');
 exports.getArtist = function getArtist(req, res) {
   res.send(artists.find({ Name: req.body.name }));
 };
+
+exports.getAllArtists = function getAllArtists(req, res) {
+  res.send(artists.data);
+};
--- app/getSong.js
+++ app/getSong.js
@@ -3,3 +3,7 @@ const { songs } = require('./database.js');
 exports.getSong = function getSong(req, res) {
   res.send(songs.find({ Genre: req.body.genre }));
 };
+
+exports.getAllSongs = function getAllSongs(req, res) {
+  res.send(songs.data);
+};

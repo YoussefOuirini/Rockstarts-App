4d5e348032907a14484499fb279c66456dd042ca
Youssef Ouirini <youssef.ouirini-cic.netherlands@ibm.com>
2019-06-16 20:18:32 +0200

    getting only metal artists and songs before 2016

--- app/database.js
+++ app/database.js
@@ -8,9 +8,13 @@ const songs = db.addCollection('songs');
 const artistsJson = require('../data/artists.json');
 const songsJson = require('../data/songs.json');
 
+const metalSongs = songsJson.filter(song => song.Genre === 'Metal');
+const metalSongsBefore2016 = metalSongs.filter(metalSong => metalSong.Year < 2016);
+const metalArtists = artistsJson.filter(artist => metalSongs.find(song => artist.Name === song.Artist));
+
 exports.importData = function importData() {
-  artists.insert(artistsJson);
-  songs.insert(songsJson);
+  artists.insert(metalArtists);
+  songs.insert(metalSongsBefore2016);
 };
 
 exports.artists = artists;
--- app/routes.js
+++ app/routes.js
@@ -2,11 +2,13 @@ const bodyParser = require('body-parser');
 
 const jsonParser = bodyParser.json();
 
-const { getArtist } = require('./getArtist.js');
-const { getSong } = require('./getSong.js');
+const { getArtist, getAllArtists } = require('./getArtist.js');
+const { getSong, getAllSongs } = require('./getSong.js');
 
 /** @desc: list API calls * */
 module.exports = (app) => {
+  app.get('/getAllArtists', jsonParser, getAllArtists);
+  app.get('/getAllSongs', jsonParser, getAllSongs);
   app.post('/getArtist', jsonParser, getArtist);
   app.post('/getSong', jsonParser, getSong);
 };

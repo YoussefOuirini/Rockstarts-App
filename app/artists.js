fd10a9cf142068d3d736584f08a56f2beb02d5b3
Youssef Ouirini <youssef.ouirini-cic.netherlands@ibm.com>
2019-06-16 20:33:04 +0200

    Adding addArtist post request

--- app/artists.js
+++ app/artists.js
@@ -7,3 +7,14 @@ exports.getArtist = function getArtist(req, res) {
 exports.getAllArtists = function getAllArtists(req, res) {
   res.send(artists.data);
 };
+
+exports.addArtist = function addArtist(req, res) {
+  // Add some verification here to check req.body
+  const artist = artists.find({ Name: req.body.Name });
+  if (artist.length > 0) {
+    res.send(`${req.body.Name} is already in the database.`);
+  } else {
+    artists.insert(req.body);
+    res.send(`${req.body.Name} added to artists.`);
+  }
+};
--- app/routes.js
+++ app/routes.js
@@ -2,13 +2,15 @@ const bodyParser = require('body-parser');
 
 const jsonParser = bodyParser.json();
 
-const { getArtist, getAllArtists } = require('./artists.js');
+const { getArtist, getAllArtists, addArtist } = require('./artists.js');
 const { getSong, getAllSongs } = require('./songs.js');
 
 /** @desc: list API calls * */
 module.exports = (app) => {
   app.get('/getAllArtists', jsonParser, getAllArtists);
-  app.get('/getAllSongs', jsonParser, getAllSongs);
   app.post('/getArtist', jsonParser, getArtist);
+  app.post('/addArtist', jsonParser, addArtist);
+
+  app.get('/getAllSongs', jsonParser, getAllSongs);
   app.post('/getSong', jsonParser, getSong);
 };
